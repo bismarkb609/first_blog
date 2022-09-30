8734425b83a716ca575841a0989f74963fc30acc
bismarkboateng <bismarkb609@gmail.com>
2022-09-30 14:03:36 +0000

    added additional codes and changes

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path("", views.index, name="home-page"),
+    path('list_posts/', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), 
+    path("sign_up/", views.sign_up, name="sign-up"),
+    path("log_in/", views.log_in, name="log-in")
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,12 @@ from django.utils import timezone
 from .models import Post
 
 # Create your views here.
+
+def index(request):
+    data = Post.objects.all()
+    ctx = {"data": data}
+    return render(request, "blog/index.html", ctx)
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -13,3 +19,11 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def sign_up():
+    pass 
+
+
+def log_in():
+    pass 
